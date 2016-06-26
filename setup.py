ab669ed77e2f758fab7a2d824dbc15c238efb5a0
Unknwon <u@gogs.io>
2016-06-27 01:25:22 +0800

    Add test data

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-link-tests',
+    version='0.0.0',
+    url='http://github.com/sgtest/python-link-tests',
+    packages=['pkg0'],
+    install_requires=[],
+)
---
+++ tests/test_m0.py
@@ -0,0 +1,3 @@
+import os
+
+TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
